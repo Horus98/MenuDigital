4ce6f11e0b88dcad368cae1b8208dbd7326b0c2d
Horus <horusmoro2015@gmail.com>
2020-08-24 20:27:30 -0300

    Starting Menu Design.

---
+++ menu.js
@@ -0,0 +1,23 @@
+
+const menu = new Vue({
+    el: '#menu',
+    data() {
+        return {
+            cervezas : [{
+                "id": 5,
+                "descripcion":"Rubia",
+                "nombre":"IPA",
+                "precio": 125.00,
+                "imagen": "https://i0.wp.com/www.buenosbares.com/wp-content/uploads/2017/10/IPA-la-nueva-especialidad-de-Cerveza-Imperial.jpg?fit=4000%2C2670"
+            },
+            {
+                "id": 2,
+                "descripcion":"Roja",
+                "nombre":"Irish",
+                "precio": 120.00,
+                "imagen": "https://i0.wp.com/www.buenosbares.com/wp-content/uploads/2017/10/IPA-la-nueva-especialidad-de-Cerveza-Imperial.jpg?fit=4000%2C2670"
+            }
+        ]
+        }
+    }
+});
