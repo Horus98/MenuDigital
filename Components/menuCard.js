19a949781d898cc46dfe1ffe4d6726a5a162ae0d
Horus <horusmoro2015@gmail.com>
2020-08-25 14:55:45 -0300

    Se añade css.
    Se Añaden botones circulares para agregar en pedidos.

--- Components/menuCard.js
+++ Components/menuCard.js
@@ -10,17 +10,18 @@ Vue.component('menu-card',{
                     alt="Card image cap">
         </div>
         <!-- Card content -->
-        <div class="card-body">
+        <div class="card-body elegant-color white-text rounded-bottom">
             <!-- Title -->
             <h4 class="card-title">IPA $150.00</h4>
             <!-- Text -->
-            <p class="card-text">Cerveza lupulada y de amargor intenso, ideal para quienes deseen
+            <p class="card-text white-text">Cerveza lupulada y de amargor intenso, ideal para quienes deseen
                 una cerveza distinta.
             </p>
             <!-- Provides extra visual weight and identifies the primary action in a set of buttons -->
-            <button type="button" class="btn btn-green btn-md " @click="cantidad++">Agregar</button>
-            <button type="button " class="btn btn-red btn-md" @click= restar >Quitar</button>
+            <button type="button" class="btn btn-green btn-sm btn-circle " @click="cantidad++"><i class="fas fa-plus"></i></button>
             <span class="text-center">Cantidad: {{cantidad}}</span>
+            <button type="button " class="btn btn-red btn-sm btn-circle"  @click= restar ><i class="fas fa-minus"></i></button>
+           
         </div>
     </div>
     </div>
