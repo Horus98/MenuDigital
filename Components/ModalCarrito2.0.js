216f4fc55bf0849fdf4f2824d4f2e841d2b775f1
Horus98 <42451015+Horus98@users.noreply.github.com>
2020-09-05 19:50:08 -0300

    Mejorando carrito alternativo.

--- Components/ModalCarrito2.0.js
+++ Components/ModalCarrito2.0.js
@@ -1,5 +1,6 @@
 Vue.component('modal-carrito-table',{
     template:`
+    <div>
     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCart">Launch modal</button>
 
 <!-- Modal: modalCart -->
@@ -9,7 +10,7 @@ Vue.component('modal-carrito-table',{
     <div class="modal-content">
       <!--Header-->
       <div class="modal-header">
-        <h4 class="modal-title" id="myModalLabel">Your cart</h4>
+        <h4 class="modal-title" id="myModalLabel">Tu pedido</h4>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
@@ -23,50 +24,28 @@ Vue.component('modal-carrito-table',{
               <th>Imagen</th>
               <th>Nombre</th>
               <th>Precio</th>
-              <th>Editar</th>
+              <th class="text-center">Cantidad</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row"><img src= "https://images.crateandbarrel.com/is/image/Crate/SpiegelauIPAGlass18ozSHS16" alt="imagen item" height="60px" width="60px" style=" object-fit: cover;"></th>
-              <td>Product 1</td>
-              <td>100$</td>
+          
+            <tr v-for= "item of $store.state.itemsSeleccionados" >
+              <th scope="row"><img :src= item.image width="40px"></th>
+              <td>{{item.name}}</td>
+              <td>{{price(item)}}</td>
               <td>
                 <div>
                     <div class="def-number-input number-input safari_only mb-0 w-100">
                       <button  
-                        class="minus"></button>
-                      <input class="quantity" min="0" name="quantity" value="1" type="number">
+                        class="minus" @click = "restar(item)"></button>
+                      <input class="quantity" min="0" name="quantity" :value= cantidad(item.id) type="number" disabled>
                       <button 
-                        class="plus"></button>
+                        class="plus" @click = "sumar(item)"></button>
                     </div>
                   </div>
-            </td>
+              </td>
             </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Product 2</td>
-              <td>100$</td>
-              <td><a><i class="fas fa-times"></i></a></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Product 3</td>
-              <td>100$</td>
-              <td><a><i class="fas fa-times"></i></a></td>
-            </tr>
-            <tr>
-              <th scope="row">4</th>
-              <td>Product 4</td>
-              <td>100$</td>
-              <td><a><i class="fas fa-times"></i></a></td>
-            </tr>
-            <tr class="total">
-              <th scope="row">5</th>
-              <td>Total</td>
-              <td>400$</td>
-              <td></td>
-            </tr>
+            
           </tbody>
         </table>
 
@@ -74,41 +53,71 @@ Vue.component('modal-carrito-table',{
       <!--Footer-->
       <div class="modal-footer">
         <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Close</button>
-        <button class="btn btn-primary">Checkout</button>
+        <button class="btn btn-primary" @click="buildOrder()" >Checkout</button>
       </div>
     </div>
   </div>
 </div>
+</div>
+
 <!-- Modal: modalCart --></div>
     `,
     data() {
-        return {
-            items: store.state.cantidadItemSeleccionado,
-            observations : "",
-        }
-    },
-    methods: {
-        clear(){
-                
-        },
-        cantidad(id) {
-            return store.state.cantidadItemSeleccionado.get(id);
-        },
-        price(item) {
-            let precio = this.cantidad(item.id) * item.price;
-            precio = new Intl.NumberFormat("de-DE").format(precio);
-            return "$" + precio;
-        },
-        sumar(item) {
-            store.commit('agregar', item);
-        },
-        restar(item) {
-            let cantidad = this.cantidad(item.id);
-            if (cantidad > 0)
-                store.commit('quitar', item);
-        }
-    },
+      return {
+          items: store.state.cantidadItemSeleccionado,
+          observations: "",
+          
+      }
+  },
+  methods: {
+      clear() {
+          this.observations = "";
+          store.commit('clear');
+      },
+      cantidad(id) {
+          return store.state.cantidadItemSeleccionado.get(id);
+      },
+      price(item) {
+          let precio = this.cantidad(item.id) * item.price;
+          precio = new Intl.NumberFormat("de-DE").format(precio);
+          return "$" + precio;
+      },
+      sumar(item) {
+          store.commit('agregar', item);
+      },
+      restar(item) {
+          let cantidad = this.cantidad(item.id);
+          if (cantidad > 0)
+              store.commit('quitar', item);
+      },
+      buildOrder() {
+          let order = {
+              "table": 1,
+              "items": Array.from(store.state.cantidadItemSeleccionado.keys()),
+              "quantities": Array.from(store.state.cantidadItemSeleccionado.values())
+          };
 
+          console.log(order);
+          axios.post('http://127.0.0.1:8000/api/orders/', order, {
+                  headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+              })
+              .then(response => {
+                  if (response.status === 200) {
+                      this.notify();
+                      this.clear();
+                  };
+              }).catch(e => {
+                  console.log(e);
+              });
+      },
+      notify() {
+          swal("Tu pedido ha sido enviado!", "Momento de esperar!", "success", {
+              timer: 3000
+          });
+      }
+  },
 
 });
 
