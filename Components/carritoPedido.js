26749ba61ae5ed50b0d55b8e632f37951eff640d
Maxi Montenegro <maximontenegro@gmail.com>
2021-08-24 21:42:07 -0300

    quito ultima barra a cada url

--- Components/carritoPedido.js
+++ Components/carritoPedido.js
@@ -1,4 +1,4 @@
-const SEND_ORDER_URL = 'http://beer-menu-iaw.herokuapp.com/api/orders/'
+const SEND_ORDER_URL = 'http://beer-menu-iaw.herokuapp.com/api/orders'
 
 Vue.component('modal-carrito-table', {
   template: `
